e6f1680228b56033dbf525a73f4c12d460811405
MandyIntheSpace <timsinarewon@gmail.com>
2023-03-25 00:06:25 +0545

    popup

---
+++ popup.js
@@ -0,0 +1,11 @@
+console.log("Hello welocme to the world of javascript")
+
+let height = screen.height
+let width = screen.width
+console.log(height)
+console.log(width)
+
+toggle = () => {
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+}
