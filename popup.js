d81487d63d8d4701f37fcf284938c9102b6e891a
MandyIntheSpace <timsinarewon@gmail.com>
2023-03-25 22:45:46 +0545

    haha

--- popup.js
+++ popup.js
@@ -5,7 +5,28 @@ let width = screen.width
 console.log(height)
 console.log(width)
 
-toggle = () => {
-    var blur = document.getElementById('blur')
-    blur.classList.toggle('active')
-}
+// toggle = () => {
+//     var blur = document.getElementById('blur')
+//     blur.classList.toggle('active')
+// }
+
+var imagefield = document.getElementById("image-field")
+var maincontainer = document.getElementById("maincontainer")
+var closeButton = document.querySelector(".fa-sharp")
+var body = document.querySelector("body")
+
+maincontainer.style.display = 'none'
+
+imagefield.addEventListener("click", ()=>{
+    // maincontainer.classList.toggle('active')
+    maincontainer.style.display = 'flex'
+    body.classList.add('active')
+    // body.style.backdropFilter = 'blur(5px)'
+})
+
+closeButton.addEventListener("click", ()=>{
+    maincontainer.style.display = "none"
+    body.classList.remove("active")
+    body.style.backdropFilter = 'none'
+})
+
